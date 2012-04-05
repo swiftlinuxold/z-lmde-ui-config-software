e8b9659d71252d3847a9d8b95fe8ec5208d5e182
Jason Hsu <github@jasonhsu.com>
2012-04-05 00:07:43 -0500

    Updated package installation function

--- main.py
+++ main.py
@@ -37,18 +37,30 @@ def install_pkg_antix (name1, name2, url):
     devnull = open (os.devnull,"w")
     retval = subprocess.call(["dpkg","-s",name1],stdout=devnull,stderr=subprocess.STDOUT)
     devnull.close()
-    if retval == 0:
-        print (name1 + ' is already installed')
-    else:
-        print 'DOWNLOADING ' + name1 + ' FROM ' + url
-        wget_command = 'wget -nv -nd -nH -r -l1 --wait=1 -erobots=off --no-parent -A '
+    i = 0
+    while (retval <> 0) and (i < 50):
+        os.system ('echo DOWNLOADING ' + name1 + ' FROM ' + url)
+        wget_command = 'wget -nv -nd -nH -r -l1 -q --no-parent -A '
         deb_file = name1 + '_*' + name2
         wget_command = wget_command + chr(39) + deb_file + chr(39) + ' '
         wget_command = wget_command + url
-        print wget_command
+        os.system ('echo ' + wget_command)
         os.system (wget_command)
         os.system ('dpkg -i ' + deb_file)
         os.system ('rm ' + deb_file)
+        os.system ('rm robot*')
+        devnull = open (os.devnull,"w")
+        retval = subprocess.call(["dpkg","-s",name1],stdout=devnull,stderr=subprocess.STDOUT)
+        devnull.close()
+        if (i > 3) and (retval <> 0):
+            os.system ('echo Installation not completed, will try again')
+            import time, random
+            sec = random.randrange (3,10)
+            time.sleep (sec)
+    if retval == 0:
+        os.system ('echo ' + name1 + ' is already installed')
+    else:
+        os.system ('echo WARNING: ' + name1 + ' is NOT installed')
 	
 src = dir_develop + '/ui-config-software/usr_local_bin/config-software.py'
 dest = '/usr/local/bin/config-software.py'
